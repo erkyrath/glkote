f73151a2b89ca68140c3258eac72e8d19288f390
Andrew Plotkin <zarf@ZarfLent.local>
2020-12-20 18:36:54 -0500

    Version number bump.

--- glkapi.js
+++ glkapi.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* GlkAPI -- a Javascript Glk API for IF interfaces
- * GlkOte Library: version 2.2.5.
+ * GlkOte Library: version 2.3.0.
  * Glk API which this implements: version 0.7.4.
  * Designed by Andrew Plotkin <erkyrath@eblong.com>
  * <http://eblong.com/zarf/glk/glkote.html>
@@ -6314,7 +6314,7 @@ function glk_date_to_simple_time_local(dateref, factor) {
 /* End of Glk namespace function. Return the object which will
    become the Glk global. */
 return {
-    version: '2.2.6', /* GlkOte/GlkApi version */
+    version: '2.3.0', /* GlkOte/GlkApi version */
     init : init,
     update : update,
     save_allstate : save_allstate,
--- glkote.js
+++ glkote.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* GlkOte -- a Javascript display library for IF interfaces
- * GlkOte Library: version 2.2.6.
+ * GlkOte Library: version 2.3.0.
  * Designed by Andrew Plotkin <erkyrath@eblong.com>
  * <http://eblong.com/zarf/glk/glkote.html>
  * 
@@ -3029,7 +3029,7 @@ function evhan_debug_command(cmd) {
 /* End of GlkOte namespace function. Return the object which will
    become the GlkOte global. */
 return {
-  version:  '2.2.6',
+  version:  '2.3.0',
   init:     glkote_init,
   inited:   glkote_inited,
   update:   glkote_update,
