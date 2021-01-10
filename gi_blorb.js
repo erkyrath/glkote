a31b9d7ca1a1e39ba06c0c24cde1c6010d198325
Andrew Plotkin <zarf@ZarfLent.local>
2021-01-09 19:29:14 -0500

    Add png/jpeg analyze functions.

--- gi_blorb.js
+++ gi_blorb.js
@@ -249,6 +249,73 @@ function get_exec_data()
     return chunk.content;
 }
     
+/* Given a PNG file, extract its dimensions. Return a {width,height}
+   object, or undefined on error. 
+*/
+function find_dimensions_png(arr) {
+    var pos = 0;
+    if (arr[0] != 0x89 || String.fromCharCode.apply(this, arr.slice(1,4)) != 'PNG') {
+        //console.log('find_dimensions_png: PNG signature does not match');
+        return undefined;
+    }
+    pos += 8;
+    while (pos < arr.length) {
+        var chunklen = (arr[pos+0] << 24) | (arr[pos+1] << 16) | (arr[pos+2] << 8) | (arr[pos+3]);
+        pos += 4;
+        var chunktype = String.fromCharCode.apply(this, arr.slice(pos,pos+4));
+        pos += 4;
+        if (chunktype == 'IHDR') {
+            var res = {};
+            res.width  = (arr[pos+0] << 24) | (arr[pos+1] << 16) | (arr[pos+2] << 8) | (arr[pos+3]);
+            pos += 4;
+            res.height = (arr[pos+0] << 24) | (arr[pos+1] << 16) | (arr[pos+2] << 8) | (arr[pos+3]);
+            pos += 4;
+            return res;
+        }
+        pos += chunklen;
+        pos += 4; /* skip CRC */
+    }
+
+    //console.log('find_dimensions_png: no PNG header block found');
+    return undefined;
+}
+
+/* Given a JPEG file, extract its dimensions. Return a {width,height}
+   object, or undefined on error. 
+*/
+function find_dimensions_jpeg(arr) {
+    var pos = 0;
+    while (pos < arr.length) {
+        if (arr[pos] != 0xFF) {
+            //console.log('find_dimensions_jpeg: marker is not 0xFF');
+            return undefined;
+        }
+        while (arr[pos] == 0xFF) 
+            pos += 1;
+        var marker = arr[pos];
+        pos += 1;
+        if (marker == 0x01 || (marker >= 0xD0 && marker <= 0xD9)) {
+            /* marker type has no data */
+            continue;
+        }
+        var chunklen = (arr[pos+0] << 8) | (arr[pos+1]);
+        if (marker >= 0xC0 && marker <= 0xCF && marker != 0xC8) {
+            if (chunklen < 7) {
+                //console.log('find_dimensions_jpeg: SOF block is too small');
+                return undefined;
+            }
+            var res = {};
+            res.height = (arr[pos+3] << 8) | (arr[pos+4]);
+            res.width  = (arr[pos+5] << 8) | (arr[pos+6]);
+            return res;
+        }
+        pos += chunklen;
+    }
+
+    //console.log('find_dimensions_jpeg: no SOF marker found');
+    return undefined;
+}
+
 /* End of Blorb namespace function. Return the object which will
    become the Blorb global. */
 return {
