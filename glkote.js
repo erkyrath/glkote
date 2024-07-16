53191b19d1d9199609c2c49f072f44eb87d37651
Andrew Plotkin <zarf@ZarfLent.local>
2024-07-16 10:55:22 -0400

    Version bump, although we've got more changes to go.

--- glkote.js
+++ glkote.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* GlkOte -- a Javascript display library for IF interfaces
- * GlkOte Library: version 2.3.3.
+ * GlkOte Library: version 2.3.4.
  * Designed by Andrew Plotkin <erkyrath@eblong.com>
  * <http://eblong.com/zarf/glk/glkote.html>
  * 
@@ -2955,7 +2955,7 @@ function evhan_debug_command(cmd) {
    become the GlkOte global. */
 return {
     classname: 'GlkOte',
-    version:  '2.3.3',
+    version:  '2.3.4',
     init:     glkote_init,
     inited:   glkote_inited,
     update:   glkote_update,
