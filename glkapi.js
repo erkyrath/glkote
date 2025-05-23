b21b5f231a4b531244ebfd6f44e69c0ad0ff65df
Andrew Plotkin <zarf@ZarfLent.local>
2025-05-23 11:21:46 -0400

    Fix typo; also explicitly ignore maxwidth for graphics windows.

--- glkapi.js
+++ glkapi.js
@@ -5590,7 +5590,7 @@ function glk_image_draw_scaled(win, imgid, val1, val2, width, height) {
 
     /* Same as above, except we use the passed-in width and height
        values. Note that by omitting winmaxwidth, we will implicitly
-       limit maxwidth to the window width. */
+       limit maxwidth to the window width (in buffer windows). */
     var img = { special:'image', image:imgid, 
                 url:info.url, alttext:info.alttext,
                 width:width, height:height };
@@ -5652,21 +5652,17 @@ function glk_image_draw_scaled_ext(win, imgid, val1, val2, width, height, imager
 
     if (win.type == Const.wintype_Graphics) {
         /* For graphics windows, we can (and should) calculate ratios
-           now. */
+           now. We ignore maxwidth. */
         if (widthrule == Const.imagerule_WidthRatio) {
             widthrule = Const.imagerule_WidthFixed;
             width = win.graphwidth * (width / 0x10000);
         }
         if (heightrule == Const.imagerule_AspectRatio) {
-            heightrule = Const.imagerule_WidthFixed;
+            heightrule = Const.imagerule_HeightFixed;
             var aspect = (info.height / info.width);
             height = width * aspect * (height / 0x10000);
         }
-        if (maxwidth && width > maxwidth*win.graphwidth) {
-            var aspect = (height / width);
-            width = maxwidth*win.graphwidth;
-            height = width * aspect;
-        }
+        maxwidth = 0;
     }
     
     switch (widthrule) {
